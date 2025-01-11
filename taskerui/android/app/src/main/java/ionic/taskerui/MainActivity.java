ea19ff1b5d32a387c02cf1d2b1557d54b7f4f868
tommytro <tocotro@gmail.com>
2025-01-10 22:37:12 -0800

    Add Tasker UI Angular/Ionic Project Template

---
+++ taskerui/android/app/src/main/java/ionic/taskerui/MainActivity.java
@@ -0,0 +1,5 @@
+package ionic.taskerui;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ taskerui/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.taskerui',
+  appName: 'taskerui',
+  webDir: 'www'
+};
+
+export default config;
